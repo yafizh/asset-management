2492adc4be2d05c22df22d520f34a27a167a56aa
A Chairil Yafizh <a.chairil.yafizh@gmail.com>
2022-10-26 04:05:34 +0700

    feature aset (index,create)

--- halaman/aset/index.php
+++ halaman/aset/index.php
@@ -3,35 +3,61 @@
         <div class="col-12">
             <div class="card my-4">
                 <div class="card-header p-0 position-relative mt-n4 mx-3 z-index-2">
-                    <div class="bg-gradient-primary shadow-primary border-radius-lg p-3 d-flex justify-content-between align-items-center">
+                    <div class="bg-gradient-success shadow-success border-radius-lg p-3 d-flex justify-content-between align-items-center">
                         <h6 class="text-white text-capitalize m-0">Data Aset</h6>
-                        <a href="?h=tambah_aset" class="btn btn-success m-0">Tambah</a>
+                        <a href="?h=tambah_aset" class="btn btn-dark m-0">Tambah</a>
                     </div>
                 </div>
-                <div class="card-body px-0 pb-2">
+                <div class="card-body pb-3">
                     <div class="table-responsive p-0">
-                        <table class="table align-items-center mb-0">
+                        <table id="datatable" class="table align-items-center mb-0">
                             <thead>
                                 <tr>
-                                    <th class="text-uppercase text-center text-secondary text-xxs font-weight-bolder opacity-7">Nama</th>
-                                    <th class="text-uppercase text-center text-secondary text-xxs font-weight-bolder opacity-7">Keterangan</th>
+                                    <th class="text-uppercase text-center text-secondary text-xs font-weight-bolder opacity-7">No</th>
+                                    <th class="text-uppercase text-center text-secondary text-xs font-weight-bolder opacity-7">Jenis Aset</th>
+                                    <th class="text-uppercase text-center text-secondary text-xs font-weight-bolder opacity-7">Jumlah</th>
+                                    <th class="text-uppercase text-center text-secondary text-xs font-weight-bolder opacity-7">Sedang Dipinjam</th>
                                     <th class="text-secondary opacity-7"></th>
                                 </tr>
                             </thead>
+                            <?php
+                            $q = "
+                                SELECT 
+                                    ja.nama AS jenis_aset, 
+                                    COUNT(a.id) AS jumlah,
+                                    (SELECT COUNT(id) FROM peminjaman_aset AS pa WHERE (pa.id_aset=a.id) AND (pa.timestamp_pengembalian_disetujui IS NULL)) AS sedang_dipinjam
+                                FROM 
+                                    aset AS a 
+                                INNER JOIN 
+                                    jenis_aset AS ja 
+                                ON 
+                                    a.id_jenis_aset=ja.id 
+                                GROUP BY 
+                                    id_jenis_aset
+                            ";
+                            $result = $mysqli->query($q);
+                            $no = 1;
+                            ?>
                             <tbody>
-                                <tr>
-                                    <td class="text-center">
-                                        <p class="text-secondary mb-0">Developer</p>
-                                    </td>
-                                    <td class="text-center">
-                                        <p class="text-secondary mb-0">Developer</p>
-                                    </td>
-                                    <td class="small-td">
-                                        <a href="?h=detail_aset&id=" class="btn btn-sm btn-info">Detail</a>
-                                        <a href="?h=edit_aset&id=" class="btn btn-sm btn-warning">Edit</a>
-                                        <a href="?h=delete_aset&id=" class="btn btn-sm btn-danger">Hapus</a>
-                                    </td>
-                                </tr>
+                                <?php while ($row = $result->fetch_assoc()) : ?>
+                                    <tr>
+                                        <td class="text-center">
+                                            <p class="text-secondary mb-0"><?= $no++; ?></p>
+                                        </td>
+                                        <td class="text-center">
+                                            <p class="text-secondary mb-0"><?= $row['jenis_aset']; ?></p>
+                                        </td>
+                                        <td class="align-middle text-center">
+                                            <span class="badge badge-sm bg-gradient-success p-2"><?= $row['jumlah']; ?></span>
+                                        </td>
+                                        <td class="align-middle text-center">
+                                            <span class="badge badge-sm bg-gradient-info p-2"><?= $row['sedang_dipinjam']; ?></span>
+                                        </td>
+                                        <td class="small-td">
+                                            <a href="?h=detail_aset&id=" class="btn btn-sm btn-info text-white">Lihat</a>
+                                        </td>
+                                    </tr>
+                                <?php endwhile; ?>
                             </tbody>
                         </table>
                     </div>
--- halaman/aset/tambah.php
+++ halaman/aset/tambah.php
@@ -1,49 +1,141 @@
 <?php
-
 if (isset($_POST['submit'])) {
-    $nama = $_POST['nama'];
-    $keterangan = $_POST['keterangan'];
+    $id_jenis_aset = $mysqli->real_escape_string($_POST['id_jenis_aset']);
+    $id_sifat_aset = $mysqli->real_escape_string($_POST['id_sifat_aset']);
+    $nama = $mysqli->real_escape_string($_POST['nama']);
+    $tanggal_masuk = $mysqli->real_escape_string($_POST['tanggal_masuk']);
+    $keterangan = $mysqli->real_escape_string($_POST['keterangan']);
+
+    $detail = [];
+    for ($i = 0; $i < count($_POST['detail']); $i += 2) {
+        $key = $mysqli->real_escape_string($_POST['detail'][$i]);
+        $value = $mysqli->real_escape_string($_POST['detail'][$i + 1]);
+        if (empty(trim($key))) continue;
+        if (empty(trim($key)) && empty(trim($value))) continue;
+        $detail[$key] = $value;
+    }
+
+    $foto = $_FILES["foto"];
+    $uploadOk = true;
+    if ($foto['error'] != 4) {
+        $target_dir = "uploads/gambar_aset/";
+        $imageFileType = strtolower(pathinfo($foto['name'], PATHINFO_EXTENSION));
+        $target_file = $target_dir . Date("YmdHis") . '.' . $imageFileType;
+        $check = getimagesize($foto["tmp_name"]);
+        if ($check === false) {
+            echo "<script>alert('File yang diupload bukan gambar!')</script>";
+            $uploadOk = false;
+        }
+
+        if (
+            $imageFileType != "jpg" && $imageFileType != "png" && $imageFileType != "jpeg"
+        ) {
+            echo "<script>alert('Hanya menerima gambar dengan format .png, .jpg, .jpeg!')</script>";
+            $uploadOk = false;
+        }
+
+        if ($uploadOk) {
+            if (!is_dir($target_dir)) mkdir($target_dir, 0700, true);
+            if (!move_uploaded_file($foto["tmp_name"], $target_file))
+                echo "<script>alert('Gagal meng-upload gambar!')</script>";
+        }
+    }
+
+    if ($uploadOk) {
+        $q = "
+            INSERT INTO aset (
+                id_jenis_aset, 
+                id_sifat_aset, 
+                nama, 
+                tanggal_masuk, 
+                detail, 
+                foto, 
+                keterangan
+            ) VALUES (
+                '$id_jenis_aset', 
+                '$id_sifat_aset', 
+                '$nama', 
+                '$tanggal_masuk', 
+                '" . json_encode($detail) . "', 
+                '$target_file',
+                '$keterangan'
+            )";
+        if ($mysqli->query($q)) {
+            echo "<script>alert('Tambah Data Berhasil!')</script>";
+            echo "<script>location.href = '?h=aset';</script>";
+        } else {
+            echo "<script>alert('Tambah Data Gagal!')</script>";
+            die($mysqli->error);
+        }
+    }
 }
-
-
 ?>
 <div class="container-fluid py-4">
     <div class="row">
         <div class="col-12">
             <div class="card my-4">
                 <div class="card-header p-0 position-relative mt-n4 mx-3 z-index-2">
-                    <div class="bg-gradient-primary shadow-primary border-radius-lg p-3 d-flex justify-content-between align-items-center">
-                        <h6 class="text-white text-capitalize m-0">Tambah Jenis Aset</h6>
+                    <div class="bg-gradient-success shadow-success border-radius-lg p-3 d-flex justify-content-between align-items-center">
+                        <h6 class="text-white text-capitalize m-0">Tambah Aset</h6>
                     </div>
                 </div>
                 <div class="card-body">
                     <form action="" method="POST" enctype="multipart/form-data">
                         <div class="mb-3">
-                            <label for="nama" class="form-label">Kode Aset</label>
-                            <div class="input-group input-group-outline">
-                                <select class="form-control" id="exampleFormControlSelect1" required>
-                                    <option selected value="" disabled>Pilih</option>
-                                    <option>Mobil</option>
-                                    <option>Kursi</option>
-                                    <option>Meja</option>
-                                    <option>Kamera</option>
-                                </select>
-                            </div>
+                            <?php
+                            $q = "SELECT * FROM jenis_aset";
+                            $result = $mysqli->query($q);
+                            ?>
+                            <label for="id_jenis_aset" class="form-label">Jenis Aset</label>
+                            <select class="form-control p-2" id="id_jenis_aset" name="id_jenis_aset" required>
+                                <option selected value="" disabled>Pilih</option>
+                                <?php while ($row = $result->fetch_assoc()) : ?>
+                                    <option value="<?= $row['id']; ?>"><?= $row['nama']; ?></option>
+                                <?php endwhile; ?>
+                            </select>
+                        </div>
+                        <div class="mb-3">
+                            <?php
+                            $q = "SELECT * FROM sifat_aset";
+                            $result = $mysqli->query($q);
+                            ?>
+                            <label for="id_sifat_aset" class="form-label">Sifat Aset</label>
+                            <select class="form-control p-2" id="id_sifat_aset" name="id_sifat_aset" required>
+                                <option selected value="" disabled>Pilih</option>
+                                <?php while ($row = $result->fetch_assoc()) : ?>
+                                    <option value="<?= $row['id']; ?>"><?= $row['nama']; ?></option>
+                                <?php endwhile; ?>
+                            </select>
                         </div>
                         <div class="mb-3">
                             <label for="nama" class="form-label">Nama</label>
-                            <div class="input-group input-group-outline">
-                                <input type="text" class="form-control" name="nama" id="nama" autocomplete="off" autofocus required>
-                            </div>
+                            <input type="text" class="form-control p-2" name="nama" id="nama" autocomplete="off" required>
+                        </div>
+                        <div class="mb-3">
+                            <label for="tanggal_masuk" class="form-label">Tanggal Masuk</label>
+                            <input type="date" class="form-control p-2" name="tanggal_masuk" id="tanggal_masuk" autocomplete="off" required value="<?= Date("Y-m-d"); ?>">
+                        </div>
+                        <div class="mb-3">
+                            <label for="foto" class="form-label">Foto</label>
+                            <input class="form-control" type="file" name="foto" id="foto" required>
                         </div>
                         <div class="mb-3">
                             <label for="keterangan" class="form-label">Keterangan</label>
-                            <div class="input-group input-group-outline">
-                                <textarea class="form-control" rows="5" id="keterangan" name="keterangan" required autocomplete="off"></textarea>
+                            <textarea class="form-control p-2" rows="5" id="keterangan" name="keterangan" required autocomplete="off"></textarea>
+                        </div>
+                        <div class="mb-3">
+                            <label for="tanggal_masuk" class="form-label">Detail</label>
+                            <div class="row" id="detail">
+                                <div class="col-6 mb-3">
+                                    <input type="text" class="form-control p-2" name="detail[]" autocomplete="off">
+                                </div>
+                                <div class="col-6 mb-3">
+                                    <input type="text" class="form-control p-2" name="detail[]" autocomplete="off">
+                                </div>
                             </div>
                         </div>
                         <div class="d-flex justify-content-between">
-                            <a href="#" class="btn btn-secondary">Kembali</a>
+                            <a href="?h=aset" class="btn btn-secondary">Kembali</a>
                             <button type="submit" name="submit" class="btn btn-primary">Tambah</button>
                         </div>
                     </form>
@@ -52,3 +144,22 @@ if (isset($_POST['submit'])) {
         </div>
     </div>
 </div>
+<script>
+    $(document).on('input', '#detail input', () => {
+        let add_detail = false;
+        $("#detail input").each((index, input) => {
+            if (input.value) add_detail = true;
+            else add_detail = false;
+        });
+        if (add_detail) {
+            $("#detail").append(`
+                <div class="col-6 mb-3">
+                    <input type="text" class="form-control p-2" name="detail[]" autocomplete="off">
+                </div>
+                <div class="col-6 mb-3">
+                    <input type="text" class="form-control p-2" name="detail[]" autocomplete="off">
+                </div>
+            `);
+        }
+    });
+</script>
