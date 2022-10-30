cad7398ed6ec306d7746481eac3a11947a211f25
A Chairil Yafizh <a.chairil.yafizh@gmail.com>
2022-10-30 22:52:11 +0700

    seeder tabel sifat aset dan tabel jenis aset

---
+++ database/seeder.sql
@@ -0,0 +1,15 @@
+INSERT INTO jenis_aset(
+    nama, 
+    keterangan
+) VALUES 
+('Kamera', 'Kamera jenis apapun yang ada di Balai Pengkajian Teknologi Pertanian Kalimantan Selatan.'),
+('Kursi', 'Kursi jenis apapun yang ada di Balai Pengkajian Teknologi Pertanian Kalimantan Selatan.'),
+('Meja', 'Meja jenis apapun yang ada di Balai Pengkajian Teknologi Pertanian Kalimantan Selatan.'),
+('Mobil', 'Mobil jenis apapun yang ada di Balai Pengkajian Teknologi Pertanian Kalimantan Selatan.');
+
+INSERT INTO sifat_aset(
+    nama, 
+    keterangan
+) VALUES 
+('Pemberian', 'Barang pemberian dari pihak manapun.'),
+('Milik Kantor', 'Barang hasil beli dengan uang kas kantor');
