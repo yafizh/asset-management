5db5730a06b8b982b5e8b5f6aca3de04bf6e76a5
yafizh <a.chairil.yafizh@gmail.com>
2023-06-24 14:49:58 +0700

    revisi database setelah bimbingan

--- database/database.sql
+++ database/database.sql
@@ -10,15 +10,17 @@ CREATE TABLE `manajemen_aset`.`jenis_aset`(
 
 CREATE TABLE `manajemen_aset`.`kategori_aset`(
     id BIGINT UNSIGNED AUTO_INCREMENT,
+    id_jenis_aset BIGINT UNSIGNED,
     nama VARCHAR(255),
-    PRIMARY KEY(id)
+    PRIMARY KEY(id),
+    FOREIGN KEY (id_jenis_aset) REFERENCES jenis_aset (id) ON DELETE CASCADE 
 );
 
 CREATE TABLE `manajemen_aset`.`pengguna`(
     id BIGINT UNSIGNED AUTO_INCREMENT,
     username VARCHAR(255) UNIQUE,
     password VARCHAR(255),
-    status ENUM('ADMIN', 'PETUGAS', 'PEGAWAI', 'PIMPINAN'),
+    status TINYINT UNSIGNED COMMENT '1 = ADMIN, 2 = PETUGAS, 3 = PEGAWAI , 4 = KEPALA BALAI',
     PRIMARY KEY(id)
 );
 
@@ -41,30 +43,28 @@ CREATE TABLE `manajemen_aset`.`pegawai`(
 
 CREATE TABLE `manajemen_aset`.`aset`(
     id BIGINT UNSIGNED AUTO_INCREMENT,
-    id_jenis_aset BIGINT UNSIGNED,
     id_kategori_aset BIGINT UNSIGNED,
     nama VARCHAR(255),
-    tanggal_masuk DATE,
-    foto VARCHAR(255),
-    status TINYINT UNSIGNED NULL DEFAULT NULL COMMENT '1 = TERSEDIA, 2 = RUSAK, 3 = HILANG, 4 = DALAM MASA PEMELIHARAAN, 5 = SEDANG DIPINJAM',
+    jumlah INT UNSIGNED,
     PRIMARY KEY(id),
-    FOREIGN KEY (id_jenis_aset) REFERENCES jenis_aset (id) ON DELETE CASCADE, 
     FOREIGN KEY (id_kategori_aset) REFERENCES kategori_aset (id) ON DELETE CASCADE 
 );
 
-CREATE TABLE `manajemen_aset`.`detail_aset` (
+CREATE TABLE `manajemen_aset`.`aset_masuk` (
     id BIGINT UNSIGNED AUTO_INCREMENT,
     id_aset BIGINT UNSIGNED,
-    kolom VARCHAR(255),
-    nilai VARCHAR(255),
+    tanggal DATE,
+    jumlah INT UNSIGNED,
+    keterangan TEXT,
     PRIMARY KEY(id),
-    FOREIGN KEY (id_aset) REFERENCES aset (id) ON DELETE CASCADE 
+    FOREIGN KEY (id_aset) REFERENCES aset (id) ON DELETE CASCADE
 );
 
 CREATE TABLE `manajemen_aset`.`aset_rusak` (
     id BIGINT UNSIGNED AUTO_INCREMENT,
     id_aset BIGINT UNSIGNED,
     tanggal DATE,
+    jumlah INT UNSIGNED,
     keterangan TEXT,
     PRIMARY KEY(id),
     FOREIGN KEY (id_aset) REFERENCES aset (id) ON DELETE CASCADE
@@ -74,6 +74,7 @@ CREATE TABLE `manajemen_aset`.`aset_hilang` (
     id BIGINT UNSIGNED AUTO_INCREMENT,
     id_aset BIGINT UNSIGNED,
     tanggal DATE,
+    jumlah INT UNSIGNED,
     keterangan TEXT,
     PRIMARY KEY(id),
     FOREIGN KEY (id_aset) REFERENCES aset (id) ON DELETE CASCADE
@@ -84,6 +85,7 @@ CREATE TABLE `manajemen_aset`.`pemeliharaan_aset` (
     id_aset BIGINT UNSIGNED,
     tanggal_mulai DATE,
     tanggal_selesai DATE,
+    jumlah INT UNSIGNED,
     keterangan TEXT,
     PRIMARY KEY(id),
     FOREIGN KEY (id_aset) REFERENCES aset (id) ON DELETE CASCADE
@@ -91,18 +93,32 @@ CREATE TABLE `manajemen_aset`.`pemeliharaan_aset` (
 
 CREATE TABLE `manajemen_aset`.`peminjaman_aset`(
     id BIGINT UNSIGNED AUTO_INCREMENT,
-    id_pegawai BIGINT UNSIGNED,
+    id_user_peminjam BIGINT UNSIGNED,
+    id_user_verifikator BIGINT UNSIGNED,
     id_aset BIGINT UNSIGNED,
-    alasan_peminjaman VARCHAR(255),
-    timestamp_pengajuan TIMESTAMP NULL DEFAULT NULL,
-    timestamp_pengajuan_ditentukan TIMESTAMP NULL DEFAULT NULL,
+    alasan TEXT,
+    tanggal_waktu_pengajuan TIMESTAMP NULL DEFAULT NULL,
+    tanggal_waktu_verifikasi TIMESTAMP NULL DEFAULT NULL,
     keterangan_pengajuan TEXT,
-    alasan_pengembalian VARCHAR(255),
-    timestamp_pengembalian TIMESTAMP NULL DEFAULT NULL,
-    timestamp_pengembalian_ditentukan TIMESTAMP NULL DEFAULT NULL,
-    keterangan_pengembalian TEXT,
-    status TINYINT UNSIGNED NULL DEFAULT NULL COMMENT '1 = MENGAJUKAN PEMINJAMAN, 2 = PENGAJUAN DITOLAK, 3 = PENGAJUAN DITERIMA, 4 = MENGAJUKAN PENGEMBALIAN, 5 = PENGEMBALIAN DITOLAK, 6 = PENGAMBALIAN DITERIMA',
+    status TINYINT UNSIGNED, 
     PRIMARY KEY(id),
-    FOREIGN KEY (id_pegawai) REFERENCES pegawai (id) ON DELETE CASCADE,
+    FOREIGN KEY (id_user_peminjam) REFERENCES pegawai (id) ON DELETE CASCADE,
+    FOREIGN KEY (id_user_verifikator) REFERENCES pegawai (id) ON DELETE CASCADE,
+    FOREIGN KEY (id_aset) REFERENCES aset (id) ON DELETE CASCADE
+);
+
+CREATE TABLE `manajemen_aset`.`pengembalian_aset`(
+    id BIGINT UNSIGNED AUTO_INCREMENT,
+    id_peminjaman_aset BIGINT UNSIGNED,
+    id_user_verifikator BIGINT UNSIGNED,
+    id_aset BIGINT UNSIGNED,
+    alasan TEXT,
+    tanggal_waktu_pengajuan TIMESTAMP NULL DEFAULT NULL,
+    tanggal_waktu_verifikasi TIMESTAMP NULL DEFAULT NULL,
+    keterangan_pengajuan TEXT,
+    status TINYINT UNSIGNED, 
+    PRIMARY KEY(id),
+    FOREIGN KEY (id_peminjaman_aset) REFERENCES peminjaman_aset (id) ON DELETE CASCADE,
+    FOREIGN KEY (id_user_verifikator) REFERENCES pegawai (id) ON DELETE CASCADE,
     FOREIGN KEY (id_aset) REFERENCES aset (id) ON DELETE CASCADE
 );
